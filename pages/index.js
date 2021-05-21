b432a51a04599197a296e307aa2d23e6e20ea880
royhobbstn <danieljtrone@gmail.com>
2021-05-21 17:12:03 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className={styles.container}>
+    
       <Head>
         <title>Create Next App</title>
       </Head>
